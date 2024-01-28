2fe9a966acd3f92746ae8e1016f271b5fc474868
Hannes Börner <hannes15@t-online.de>
2024-01-28 14:42:20 +0100

    added Dragon

---
+++ godot/Enemies/Dragon/Dragon_Discard.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+using Godot.Collections;
+
+public partial class Dragon_discard : Enemy_Action
+{
+	[Export] public int block = 6;
+
+	public override void perform_action()
+	{
+		GD.Print(_enemy);
+
+		if (_enemy == null)
+		{
+			return;
+		}
+
+		Block_Effect block_eff = new Block_Effect();
+		block_eff.amount = block;
+		Array<Node> targets = new Array<Node>();
+		targets.Add(_enemy);
+		block_eff.execute(targets);
+	}
+}
